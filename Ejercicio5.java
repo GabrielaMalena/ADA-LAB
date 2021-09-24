d202b9fc9ea1a77b473313a0250727adad4162ff
Gabriela Quispe <gquispequispe@unsa.edu.pe>
2021-09-23 21:30:11 -0500

    Agregue mi ejercicio 5 del Aula 1

---
+++ Ejercicio5.java
@@ -0,0 +1,27 @@
+package AULA1;
+import java.util.Scanner;
+
+public class Ejercicio5 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Ingrese lado de su figura *: ");
+		int lado=sc.nextInt();
+		sc.close();
+
+		System.out.println();
+		for(int i=1; i<=lado; i++){
+			for(int j=1; j<=lado; j++){
+				if (i==1 || i==lado || j==1 || j==lado){
+					System.out.print("*");
+
+				}
+				else{
+					System.out.print(" ");
+				}
+
+			}
+			System.out.println();
+		}
+	}
+}
+
