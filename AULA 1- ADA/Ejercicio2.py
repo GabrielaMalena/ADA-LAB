757a37f83f28cc98ea2efc1b554fa6892e23167c
GabrielaMalena <83074491+GabrielaMalena@users.noreply.github.com>
2021-09-24 20:53:44 -0500

    descripción ejercicio

--- AULA 1- ADA/Ejercicio2.py
+++ AULA 1- ADA/Ejercicio2.py
@@ -1,4 +1,11 @@
+#Ejercicio 2
+# El arreglo lee n numeros
+# los muestra de manera inversa
+
 numeros=[1,3,5,7,9]
 print("El arreglo es: ", numeros)
 numeros.reverse()
 print("Nuestro arreglo inverso seria: ", numeros)
+
+
+
