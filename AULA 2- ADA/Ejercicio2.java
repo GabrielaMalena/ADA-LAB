da9bb00b0a9134661a6a24a115cc595d1a635474
GabrielaMalena <83074491+GabrielaMalena@users.noreply.github.com>
2021-09-24 19:22:59 -0500

    Agregue caso prueba

--- AULA 2- ADA/Ejercicio2.java
+++ AULA 2- ADA/Ejercicio2.java
@@ -1,13 +1,16 @@
 
 import java.util.Scanner;
+//Busqueda Secuencial
+//En este ejercicio enviamos un arreglo y buscamos la posicion
+// si es verdadero devuelve true de lo contrario false
+
 public class Ejercicio2 {
- 
     public static void main(String[] args) {
         int [] num = {2,4,1,5,9,12,33}; //nuestro arreglo
         Scanner scan = new Scanner(System.in);
         boolean presente= false; // si el numero existe
          
-        System.out.println("Ingrese número:"); // indicamos num
+        System.out.println("Ingrese nÃºmero:"); // indicamos num
         int encontrarNum = scan.nextInt();      
         for(int x = 0; x < num.length; x++){  
             if(num[x]== encontrarNum){  
@@ -23,3 +26,8 @@ public class Ejercicio2 {
         }
     }
 }
+
+
+//Entrada 
+//Ingrese numero: 12
+//Salida: true, posicion 6
