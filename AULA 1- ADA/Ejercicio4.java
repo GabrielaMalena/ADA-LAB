70f7fe216f60711f13cb8e8a11c0843b92e4101f
GabrielaMalena <83074491+GabrielaMalena@users.noreply.github.com>
2021-09-24 21:04:24 -0500

    Update Ejercicio4.java

--- AULA 1- ADA/Ejercicio4.java
+++ AULA 1- ADA/Ejercicio4.java
@@ -5,10 +5,10 @@ package AULA1;
 
 public class Ejercicio4 {
   public static void main(String[] args) {
-		int numeros[]={9,5,8};
-		System.out.println("Ingresamos nuestro arreglo:" + Arrays.toString(numeros));
-		Arrays.sort(numeros);
-		System.out.println("Muestra nuestro arreglo Ordenado Ascendente: " + Arrays.toString(numeros));
+      int numeros[]={9,5,8};
+      System.out.println("Ingresamos nuestro arreglo:" + Arrays.toString(numeros));
+      Arrays.sort(numeros);
+      System.out.println("Muestra nuestro arreglo Ordenado Ascendente: " + Arrays.toString(numeros));
 	}
 
 }
