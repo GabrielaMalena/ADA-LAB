53475670c17774585e537b0483ec8b448de5c935
GabrielaMalena <83074491+GabrielaMalena@users.noreply.github.com>
2021-10-04 18:38:37 -0500

    agregue entrada-salidad

--- AULA 2- ADA/Ejercicio1.c
+++ AULA 2- ADA/Ejercicio1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* Ejercicio 1 que indico la cantidad de filas 
-y el siguiente los multiplos*/
+y muestra los integers divisibles por k*/
 
 int main(void) {
  int n, k, c, contador=0; 
@@ -13,3 +13,15 @@ int main(void) {
  printf("%d", contador);
  return 0;
 }
+
+
+/* ENTRADA:
+7 3
+1
+51
+966369
+7
+9
+999996
+11
+SALIDA: 4 */
