5aa2b736411146f2473108e8cef1b206ec4d67ad
Gabriela Quispe <gquispequispe@unsa.edu.pe>
2021-11-12 15:24:31 -0600

    agregue mi clase dfs aun falta implementar

---
+++ AULA 6- ADA/dfs.java
@@ -0,0 +1,14 @@
+
+public class dfs {
+	/*
+	 * en este ejercicio se pide ingresar una matriz, y apartir
+	 * de un punto, se van pintando los otros puntos. Para lo cual
+	 * ingresamos una matriz, y una coordenada y se pinta el area circundante
+	 * y como salida nos da una matriz
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+}
